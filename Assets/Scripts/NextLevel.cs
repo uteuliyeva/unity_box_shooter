af19eac5040edf0157695fe158c2016ad0a6e2ae
Malika Uteuliyeva <37931254+uteuliyeva@users.noreply.github.com>
2022-12-02 22:48:44 +0100

    Initial commit

---
+++ Assets/Scripts/NextLevel.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class NextLevel : MonoBehaviour {
+	public string LevelToLoad;
+	// respond on collisions
+	void OnCollisionEnter(Collision newCollision)
+	{
+		// only do stuff if hit by a projectile
+		if (newCollision.gameObject.tag == "Projectile") {
+			
+			SceneManager.LoadScene(LevelToLoad);
+		}
+	}
+}
+
